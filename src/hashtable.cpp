abf0a22417e355ead17ed9add8eea48418b052be
ANDREAS XARALAMPOUS <sdi1500195@linux27.di.uoa.gr>
2018-10-18 14:50:28 +0300

    HashFunctions

--- src/hashtable.cpp
+++ src/hashtable.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include <ctime>
+
 #include "hashtable.h"
 
 using namespace std;
 
-hashFunction::hashFunction(){
-    srand(time(NULL));
-    t = rand() % W;
+euclideanHF::euclideanHF(){
+	random_device rd;
 
-    for(int i = 0; i < D; i++){
-        
-    }
+	mt19937 gen(rd());
+
+	for(int i = 0; i < D; i++){
+		normal_distribution<float> nd_random(0.0, 1.0);
+		
+		v[i] = nd_random(gen);
+	}
+
+	for (int i = 0; i < D; i++)
+		cout << i << ". " << v[i] << endl;
+
+	uniform_real_distribution<> real_random(0.0, W);
+
+	t = real_random(gen);
+
+	cout << "T: " << t << endl;
+
+}
+
+int euclideanHF::getValue(){
+	return 1;
 }
--- src/hashtable.h
+++ src/hashtable.h
@@ -4,12 +4,27 @@
 #include "dataset.h"
 
 #define W 4 // window
+
+/* Abstract class of hash Function */
 class hashFunction{
     private:
-        int t;
-        std::array<float, D> v;
+
     public:
-        hashFunction();
+       	virtual int getValue() = 0;
+};
+
+/* Hash function for euclidean distance(LSH) */ 
+class euclideanHF: public hashFunction{
+	private:
+		std::array<float, D> v; // random vector
+		float t; // random displacement
+	
+	public:
+		/* Con-De structor */
+		euclideanHF();
+		int getValue();
+		 
+
+};
 
 
-}
